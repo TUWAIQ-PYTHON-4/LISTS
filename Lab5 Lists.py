8eaf2790bc76b7c8efd2c744769bc5c9515725e1
Aaboassoud <a.aboassoud@gmail.com>
2022-05-17 11:30:14 +0300

    lab 5 solved version2

--- Lab5 Lists.py
+++ Lab5 Lists.py
@@ -1,18 +1,8 @@
-numbers = [2 ,6,5 ,6 ,25, 9 ,99 , 64]
-suming_list = sum(numbers)
-print(suming_list)
-largest_value = max(numbers)
-print(largest_value)
-odds_numbers = []
+numbers = [2 ,6,5 ,6 ,25, 9 ,99 , 64]; suming_list = sum(numbers); print(suming_list)
 
-def listOfOddNumbers(a,b,c):
-    if a % 2 == 0:
-        a = a + 1
-    for x in range(a, b, c):
-        global odds_numbers
-        odds_numbers.append(x)
-    return odds_numbers
-print(listOfOddNumbers(1200,2000,125))
+largest_value = max(numbers); print(largest_value)
 
-new_list = odds_numbers[0:5]
-print(new_list)
+odds_numbers = [n for n in range(1200,2000,125) if n%2 != 0]; print(odds_numbers)
+
+
+new_list = numbers[0:5]; print(new_list)
