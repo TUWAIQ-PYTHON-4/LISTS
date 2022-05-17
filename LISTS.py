b7884a1039fe812e25e901eeebc67ff6df07a2f6
RubaALmohya <ruba12399@gmail.com>
2022-05-17 10:36:55 +0300

    modified the Q4

--- LISTS.py
+++ LISTS.py
@@ -13,16 +13,18 @@ largest_number()
 
 
 def odd_number(num1:int , num2:int):
-    new_list=[]
+    list_odd =[]
     for i in range(num1,num2+1,125):
         if (i%2!=0):
-            new_list.append(i)
-    print("the odd number list is :",new_list)
+           list_odd.append(i)
+    print("the odd number list is :",list_odd)
+    #Q4: use list slicing to get a new list from the previous list starting from the start to the 5th element in the list
+    new_list = list_odd[:5]
+    print("the new slicing list is :" ,new_list)
+
+
 odd_number(1200,2000)     
 
-def slicing_list():
-    new_list = my_list[:5]
-    print("the new slicing list is :" ,new_list)
-slicing_list()
+
 
             
