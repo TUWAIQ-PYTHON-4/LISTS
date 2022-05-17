d74d83fa12f6827a4bfeb2084b0f41aac71c218b
Hawra Alomani <ha0073@mix.wvu.edu>
2022-05-17 11:01:32 +0300

    updated Q1 and Q2 as functions

--- lists_lab.py
+++ lists_lab.py
@@ -1,14 +1,22 @@
 # Create a new list containing several different integers .
 my_list=[11,9,13,50,33,44,20,10]
 # Q1: Write a Python program to sum all the items in the list.
-sum=0
-for element in my_list:
-    sum+= element
-print("the sum is: ", sum)
+def sum_of_list(user_list):
+    sum=0
+    for element in user_list:
+        sum+= element
+    print("the sum is: ", sum)
+
+# calling the function
+sum_of_list(my_list)
+
 
 # Q2: Write a Python program to get the largest number from the list.
-largest_number=max(my_list)
-print("the largest number is: ",largest_number)
+def largest_num(user_list):
+    largest_number=max(user_list)
+    print("the largest number is: ",largest_number)
+# calling the function
+largest_num(my_list)
 
 # Q3: Create an odd numbers list from the elements of a range from 1200 to 2000 with steps of 125, using list comprehension.
 elements_range_list=range(1200,2000,125)
