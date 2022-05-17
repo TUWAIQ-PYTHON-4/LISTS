74cdf52af96832d8f989e95c4b1909a189afacd7
Hawra Alomani <ha0073@mix.wvu.edu>
2022-05-17 10:40:30 +0300

    finised list lab

---
+++ lists_lab.py
@@ -0,0 +1,19 @@
+# Create a new list containing several different integers .
+my_list=[11,9,13,50,33,44,20,10]
+# Q1: Write a Python program to sum all the items in the list.
+sum=0
+for element in my_list:
+    sum+= element
+print("the sum is: ", sum)
+
+# Q2: Write a Python program to get the largest number from the list.
+largest_number=max(my_list)
+print("the largest number is: ",largest_number)
+
+# Q3: Create an odd numbers list from the elements of a range from 1200 to 2000 with steps of 125, using list comprehension.
+elements_range_list=range(1200,2000,125)
+odd_number_list=[num for num in elements_range_list if not(num%2==0)]
+print(odd_number_list)
+# Q4: use list slicing to get a new list from the previous list starting from the start to the 5th element in the list.
+new_list= my_list[:5]
+print(new_list)
