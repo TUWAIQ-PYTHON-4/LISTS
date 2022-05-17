bbf47df1228e9c710222f43be13d729019906495
mhnd <mhnd@hno.com>
2022-05-17 13:16:37 +0300

    added lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+my_list=[1,5,7,9,6,15]
+print(sum(my_list))
+
+my_list2=[2,8,6,8,7,41,21]
+print(max(my_list2))
+
+
+
+
+    
+odd_numbers= [n for n in range(1200,2000,125)if n%2 !=0]
+print(odd_numbers)
+
